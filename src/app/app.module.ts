ed748398e3296dcd794e2d851482c4e85a9d57d2
chetan20 <arenachetan@gmail.com>
2022-02-14 18:18:23 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppMaterialModule } from './app.material.module';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field/form-field';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppMaterialModule],
+  imports: [BrowserModule, BrowserAnimationsModule,FormsModule, ReactiveFormsModule, AppMaterialModule],
   declarations: [AppComponent, HelloComponent],
+  
   bootstrap: [AppComponent],
 })
 export class AppModule {}
